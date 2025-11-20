d62ba657832bc526aab7ffc93a1b29db2e4bdf14
Prem Prakash <ne.premprakash53@gmail.com>
2025-11-21 02:42:40 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { connectDB } from "./config/connectDB.js";
+dotenv.config();
+import userRoutes from "./routes/user.routes.js";
+import sellerRoutes from "./routes/seller.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
+import addressRoutes from "./routes/address.routes.js";
+import orderRoutes from "./routes/order.routes.js";
+
+import { connectCloudinary } from "./config/cloudinary.js";
+
+const app = express();
+
+await connectCloudinary();
+// allow multiple origins
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",  // <-- ADD THIS
+  "http://127.0.0.1:5173",
+  "http://127.0.0.1:5174"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
+
+app.use(cookieParser());
+app.use(express.json());
+
+// Api endpoints
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRoutes);
+app.use("/api/seller", sellerRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/address", addressRoutes);
+app.use("/api/order", orderRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
